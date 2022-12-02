6a263515e080f36539680a82acb5326fa63f40c3
fibre-ether <fire.breather@rediff.com>
2022-12-02 19:54:50 +0530

    remove warnings and strictmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,49 @@
-import logo from './logo.svg';
-import {useState, useRef, useEffect} from 'react'
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([])
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then((response) => response.json())
-  .then((json) => {setTodos(state=> [...state, json.title])});
-  },[])
-  const inputRef = useRef()
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => {
+        setTodos((state) => [...state, json.title]);
+      });
+  }, []);
+  const inputRef = useRef();
   return (
     <div className="container-wrapper">
-    <div className="container">
-      <input className="todo-text" placeholder={"Learn React..."} ref={inputRef}/>
-      <button className="todo-button"
-      onClick={async()=>{if(inputRef.current.value.trim()!==""){await setTodos(state => [...state, inputRef.current.value]);
-        inputRef.current.value=""
-        }
-      }}
-      >
+      <div className="container">
+        <input
+          className="todo-text"
+          placeholder={"Learn React..."}
+          ref={inputRef}
+        />
+        <button
+          className="todo-button"
+          onClick={async () => {
+            if (inputRef.current.value.trim() !== "") {
+              await setTodos((state) => [...state, inputRef.current.value]);
+              inputRef.current.value = "";
+            }
+          }}>
           +
-      </button>
-    </div>
-    {todos.map((item, index)=>{
-      return (
-      <div key={index} className="container">
-        <div className="todo-text">
-          {item}
-        </div>
-        <button className="todo-button"
-        onClick={()=>{setTodos(state => state.filter((i)=>(i!==item)))}}
-        >
-          -
         </button>
       </div>
-      )
-    })}
+      {todos.map((item, index) => {
+        return (
+          <div key={index} className="container">
+            <div className="todo-text">{item}</div>
+            <button
+              className="todo-button"
+              onClick={() => {
+                setTodos((state) => state.filter((i) => i !== item));
+              }}>
+              -
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
